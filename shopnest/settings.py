bcdcf3758d209d83d04d806b402de2d43aaec291
Nathaniel81 <nathanield356@gmail.com>
2024-05-01 23:03:22 +0300

    nc

--- shopnest/settings.py
+++ shopnest/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-9!xlw=l9cze5xot!(j2bf=06$-a1694mshoskdqv7#0li^v@st
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://shopnest-eykw.onrender.com']
+ALLOWED_HOSTS = ['shopnest-eykw.onrender.com']
 
 
 # Application definition
