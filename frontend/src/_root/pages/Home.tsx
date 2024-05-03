83dc745e392154720974bf8fb30461260fe6af5b
Nathaniel81 <nathanield356@gmail.com>
2024-05-03 12:25:38 +0300

    Completed the implementation of front page components, including product listing, layout, and styling

--- frontend/src/_root/pages/Home.tsx
+++ frontend/src/_root/pages/Home.tsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+import Banner from "../../components/shared/Banner";
+import Products from "../../components/shared/Products";
 
 const Home = () => {
-  return (
-	<div className=''>Home</div>
-  )
-}
+  const [productData, setProductData] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
+      const data = await res.json();
+      setProductData(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <main>
+      <div className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <div className="relative md:-mt020 lgl:-mt-32 xl:-mt-60 z-20 mb-10">
+          <Products productData={productData} />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
