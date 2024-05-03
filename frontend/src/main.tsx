9d3556c3e920ab8cf30a937a61dac5c1aefd66c5
Nathaniel81 <nathanield356@gmail.com>
2024-05-03 18:05:43 +0300

    Configured Redux Persist store and added Redux state for cart items

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.tsx';
-import store from './redux/store.ts';
+import { store, persistor } from './redux/store.ts';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
-)
+);
