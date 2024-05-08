eefe090a143f8f86ea0a78b2d0b4d1fb60814f0c
Nathaniel81 <nathanield356@gmail.com>
2024-05-08 15:39:03 +0300

    Update Product model

--- core/models.py
+++ core/models.py
@@ -16,18 +16,13 @@ class Product(models.Model):
     user = models.ForeignKey(User, related_name='products', on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     main_image = CloudinaryField('image', null=True, blank=True)
-    image_1 = CloudinaryField('image', null=True, blank=True)
-    image_2 = CloudinaryField('image', null=True, blank=True)
-    image_3 = CloudinaryField('image', null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.ForeignKey(Category, related_name='products', max_length=200, null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField(null=True, blank=True)
-    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    numReviews = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
+    saved_by = models.ManyToManyField(User, related_name='saved_products', blank=True)
 
     @property
     def is_new(self):
