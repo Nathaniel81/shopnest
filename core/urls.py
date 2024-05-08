1c11c49920c6e450650f9a3027b6f658edd90507
Nathaniel81 <nathanield356@gmail.com>
2024-05-08 14:28:57 +0300

    Add endpoints for saving products

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.ProductsAPIView.as_view(), name='products'), 
     path('search/', views.SearchProductsAPIView.as_view(), name='search_posts'),
+    path('categories/', views.CategoriesAPIView.as_view(), name='categories'),
+    path('<str:pk>/save/', views.SaveProductView.as_view(), name='save_product'),
+    path('<str:pk>/unsave/',views.UnsaveProductView.as_view(), name='unsave_product'),
     path('<str:pk>/', views.ProductDetailAPIView.as_view(), name='product_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
-from .models import Product
-from .serializers import ProductSerializer
-from rest_framework import generics
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+from rest_framework import generics, status
 from django.db.models import Q
+from core.authenticate import CustomAuthentication
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 class ProductsAPIView(generics.ListAPIView):
     """
@@ -10,8 +13,25 @@ class ProductsAPIView(generics.ListAPIView):
     This view returns a list of all products.
     """
 
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        category = self.request.query_params.get('category', '')
+        if category:
+            queryset = Product.objects.filter(Q(category__name__iexact=category))
+        else:
+            queryset = Product.objects.all()
+        return queryset
+
+class CategoriesAPIView(generics.ListAPIView):
+    """
+    View for listing products.
+
+    This view returns a list of all products.
+    """
+
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     """
@@ -27,18 +47,67 @@ class SearchProductsAPIView(generics.ListAPIView):
     """
     View for searching products.
 
-    This view returns a list of products filtered by a search query.
+    This view returns a list of products filtered by a search query and category.
     """
 
     serializer_class = ProductSerializer
 
     def get_queryset(self):
         """
-        Get the queryset of products filtered by a search query.
+        Get the queryset of products filtered by a search query and category.
 
-        This method filters the queryset to include only products containing the search query in their caption.
+        This method filters the queryset to include only products containing the search query in their name and belonging to the specified category.
         """
 
         query = self.request.query_params.get('query', '')
         queryset = Product.objects.filter(Q(name__icontains=query))
+        category = self.request.query_params.get('category', '')
+        if category:
+            queryset = queryset.filter(category__name__iexact=category)
         return queryset
+
+class SaveProductView(generics.GenericAPIView):
+    """
+    View for saving a product.
+
+    This view allows authenticated users to save a product.
+    """
+
+    authentication_classes = [CustomAuthentication]
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handle POST request for saving a product.
+
+        This method adds the authenticated user to the 'saved_by' field of the product.
+        """
+
+        if request.user.is_anonymous:
+            raise PermissionDenied("You must be logged in to save a product")
+
+        product = get_object_or_404(Product, id=self.kwargs.get('pk'))
+        product.saved_by.add(request.user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UnsaveProductView(generics.GenericAPIView):
+    """
+    View for unsaving a product.
+
+    This view allows authenticated users to unsave a product.
+    """
+
+    authentication_classes = [CustomAuthentication]
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handle POST request for unsaving a product.
+
+        This method removes the authenticated user from the 'saved_by' field of the product.
+        """
+
+        if request.user.is_anonymous:
+            raise PermissionDenied("You must be logged in to unsave a product")
+
+        product = get_object_or_404(Product, id=self.kwargs.get('pk'))
+        product.saved_by.remove(request.user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
