add1e62593a7e40734e751e6b3de11bdc920ac21
Nathaniel81 <nathanield356@gmail.com>
2024-05-10 20:11:23 +0300

    Add countInStock field to Product model

--- core/models.py
+++ core/models.py
@@ -17,9 +17,11 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     main_image = CloudinaryField('image', null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
-    category = models.ForeignKey(Category, related_name='products', max_length=200, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        Category, related_name='products', max_length=200, null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
     saved_by = models.ManyToManyField(User, related_name='saved_products', blank=True)
@@ -46,19 +48,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
-class Review(models.Model):
-    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200, null=True, blank=True)
-    rating = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, default=0)
-    comment = models.TextField(null=True, blank=True)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.rating)
-
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
@@ -84,14 +73,3 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.name)
-
-class ShippingAddress(models.Model):
-    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
-    address = models.CharField(max_length=200, null=True, blank=True)
-    city = models.CharField(max_length=200, null=True, blank=True)
-    postalCode = models.CharField(max_length=200, null=True, blank=True)
-    country = models.CharField(max_length=200, null=True, blank=True)
-    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-
-    def __str__(self):
-        return str(self.address)
