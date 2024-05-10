11188c351358e3f7ad80db160bf295d10561de07
Nathaniel81 <nathanield356@gmail.com>
2024-05-10 20:21:31 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Category, Product, Review, Order, OrderItem
+from .models import Category, Product, Order, OrderItem
 
 admin.site.register(Category)
 admin.site.register(Product)
-admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(OrderItem)
