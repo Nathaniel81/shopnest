80edadd71db3659781d7bc35ccd0a8d28663ca4b
Nathaniel81 <nathanield356@gmail.com>
2024-05-11 15:32:52 +0300

    Remove profile_picture & address field from User model

--- accounts/models.py
+++ accounts/models.py
@@ -6,8 +6,6 @@ from cloudinary.models import CloudinaryField
 class User(AbstractUser):
     username = models.CharField(max_length=40, unique=True)
     email = models.EmailField(unique=True)
-    profile_picture = CloudinaryField('image', null=True, blank=True)
-    address = models.TextField(null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
